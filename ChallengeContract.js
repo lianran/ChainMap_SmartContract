64ee0e15c7347104b3ac68dbd6d98dbef0a1806a
li <lianran@yeah.net>
2018-06-24 23:11:09 +0800

    update

--- ChallengeContract.js
+++ ChallengeContract.js
@@ -1,4 +1,10 @@
 'use strict';
+//Global define
+var Diamond = new BigNumber(20);
+var Gold = new BigNumber(10);
+var Silver = new BigNumber(3);
+var Brozen = new  BigNumber(1);
+var blockLimit = new BigNumber(10);
 
 var Allowed = function (obj) {
     this.allowed = {};
@@ -36,6 +42,7 @@ var ChallengeContent = function(text) {
          this.timeEstimation = o.timeEstimation.tostring();
          this.author = o.author.toString();
          this.timeStamp = o.timeStamp.toString();
+         this.blockHeight = new BigNumber(o.blockHeight);
          this.answer = new Array();
     }
 };
@@ -44,8 +51,6 @@ ChallengeContent.prototype = function(){
     // body...
 };
 
-
-//we maybe should reward the voters, so we should record voters of answer.
 var answerContent = function(text) {
     if (text) {
         var o = JSON.parse(text);
@@ -53,21 +58,37 @@ var answerContent = function(text) {
         this.answer = o.answer.toString();
         this.answered = o.answered.toString();
         this.timeStamp = o.timeStamp.toString();
-        this.like = new BigNumber(0);
-        this.dislike = new BigNumber(0);
+        this.blockHeight = new BigNumber(o.blockHeight);
+        this.like = new Array();
+        this.dislike = new Array();
     }
     else {
 
     }
 };
 
+answerContent.prototype = function(){
+    // body...
+};
+
+// var chooseContent = function (text) {
+//     if (text){
+//         var o = JSON.parse(text);
+//         this.amount = new BigNumber(o.amount);
+//         this.chooser = o.chooser.toString();
+//     }
+// };
+//
+// chooseContent.prototype=function(){
+//
+// };
+
 var challengeContract = function () {
-
-
     LocalContractStorage.defineProperties(this, {
         _name: null,
         _symbol: null,
-        //_decimals: null,
+        _decimals: null,
+        _admin: "n1WJaKuQ8vQ8AzoGBZfYH3GLGzULQ2WwJvp",
         _totalSupply: {
             parse: function (value) {
                 return new BigNumber(value);
@@ -108,11 +129,11 @@ var challengeContract = function () {
 
 challengeContract.prototype = {
 
-    init: function () {
-        this._name = "ChainMap";
-        this._symbol = "CMAP";
-        //this._decimals = 2 || 0;
-        this._totalSupply = new BigNumber(10).pow(8);//.mul(new BigNumber(10).pow(this._decimals));
+ init: function (name, symbol, decimals, totalSupply) {
+        this._name = name;
+        this._symbol = symbol;
+        this._decimals = decimals || 0;
+        this._totalSupply = new BigNumber(totalSupply).mul(new BigNumber(10).pow(decimals));
 
         var from = Blockchain.transaction.from;
         this.balances.set(from, this._totalSupply);
@@ -130,9 +151,9 @@ challengeContract.prototype = {
     },
 
     // Returns the number of decimals the token uses
-    // decimals: function () {
-    //     return this._decimals;
-    // },
+    decimals: function () {
+        return this._decimals;
+    },
 
     totalSupply: function () {
         return this._totalSupply.toString(10);
@@ -261,6 +282,7 @@ challengeContract.prototype = {
         challengeItem.timeEstimation = timeEstimation;
         challengeItem.author = from;
         challengeItem.timeStamp = new Date();
+        challengeItem.blockHeight = Blockchain.block.height;
 
         this.ChallengeValut.put(challengeId,challengeItem);
     },
@@ -277,6 +299,7 @@ challengeContract.prototype = {
         answerItem.answer = answer;
         answerItem.answered = from;
         answerItem.timeStamp = new Date();
+        answerItem.blockHeight = Blockchain.block.blockHeight;
 
         //challengeItem.answer.push(answerItem);
 
@@ -290,23 +313,24 @@ challengeContract.prototype = {
 
     VoteAnswer: function(challengeId,answerId,result){
 
-        var from = Blockchain.transaction.from;
+        var voter = Blockchain.transaction.from;
 
-        var choose = new BigNumber(result);
+        var choose = result.toString();
 
         var answerItem = this.ChallengeValut.get(challengeId).answer;
 
         for(var j = 0, length2 = answerItem.length; j < length2; j++){
 
-            if (answerItem[j].answerId == answerId){
+            if (answerItem[j].answerId === answerId){
 
                 if (choose){
                     //answerItem[j].like = answerItem[j].like + 1;
-                    this.ChallengeValut.put(challengeId,answerItem[j].like.add(choose))
+
+                    this.ChallengeValut.put(challengeId,answerItem[j].like.push(voter));
                 }
                 else {
 
-                    this.ChallengeValut.put(challengeId,answerItem[j].dislike.sub(choose))
+                    this.ChallengeValut.put(challengeId,answerItem[j].dislike.push(voter));
                 }
 
                 break;
@@ -314,52 +338,173 @@ challengeContract.prototype = {
         }
 
     },
-
-    sortStamp: function(a,b){
-
-        return a.timeStamp - b.timeStamp
-    },
-
-    //reward trigger - only challenge post and answer get reward which is triggered by the challenge poster accepts the challenge
-    //So maybe we need somechecks?
-    RewardAnswer: function(challengeId) {
-
-        var answerItem = this.ChallengeValut.get(challengeId).answer;
-
-        answerItem.sort(sortStamp);
-
-        for(var j = 0, length2 = 2; j < length2; j++){
-
-            if (answerItem[j].like > 2)
-             {
-                Event.Trigger("ChallengeValut", {
-                    Transfer: {
-                        from: Blockchain.transaction.from,
-                        to: answerItem[j].answered,
-                        value: Blockchain.transaction.value
-                    }
-                });
-             }
+    sortLike: function(a,b){
+        if (a.like.length < b.like.length) {
+                return 1;
         }
-
+        else {
+                return -1;
+        }
     },
+    tokenTansfer:function (_from,_to, _value) {
 
-    RewardChallenge: function(challengeId) {
+        this.ChallengeValut.transferFrom(_from,_to,_value);
 
-        var answerItem = this.ChallengeValut.get(challengeId).answer;
-        var author = this.ChallengeValut.get(challengeId).author;
+        Event.Trigger("ChallengeValut", {
+            Transfer: {
+                from: _from,
+                to: _to,
+                value: _value
+            }
+        });
+    },
+    RewardAll: function (challengeId) {
 
-        if (answerItem.length > 2){
-            Event.Trigger("ChallengeValut", {
-                Transfer: {
-                    from: Blockchain.transaction.from,
-                    to: author,
-                    value: Blockchain.transaction.value
+        var from = Blockchain.transaction.from;
+        var challengeItem = this.ChallengeValut.get(challengeId);
+        var answerItem = challengeItem.answer;
+        var limit = Blockchain.block.blockHeight.sub(challengeItem.blockHeight);
+        var rewardAmount = new BigNumber(0);
+
+        answerItem.sort(this.ChallengeValut.sortLike);
+
+        if (from !== challengeItem.author) {
+            throw new Error("Only for challenge developer");
+        }
+        if (answerItem.length === 0){
+            throw new Error("No answer");
+        }
+        if (limit > blockLimit) {
+            if (challengeItem.challengeLevel === "Diamond") {
+                rewardAmount = Diamond;
+            }
+            else if (challengeItem.challengeLevel === "Gold"){
+                rewardAmount = Gold;
+            }
+            else if (challengeItem.challengeLevel === "Silver"){
+                rewardAmount = Silver;
+            }
+            else if (challengeItem.challengeLevel === "Brozen"){
+                rewardAmount = Brozen;
+            }
+            var firstLevel = Math.ceil(rewardAmount*0.25);
+            var secondLevel = Math.ceil(rewardAmount*0.15);
+            var thirdLevel = Math.ceil(rewardAmount*0.1);
+            var forthLevel = rewardAmount - firstLevel - secondLevel - thirdLevel - forthLevel;
+            var answerAmount = new BigNumber(0);
+            for (var j = 0; j < answerItem.length; j++ ){
+                var answerLimit = answerItem[j].blockHeight - challengeItem.blockHeight;
+                if(answerLimit > limit) {
+                    answerAmount++;
                 }
-            });
-        }
+            }
 
+            var firstSize = Math.floor(answerAmount*0.2);
+            var secondSize = Math.floor(answerAmount*0.4);
+            var thirdSize = answerAmount - firstSize - secondSize;
+
+            for (var j_1 = 0; j_1 < firstSize; j_1++){
+                var to_1 = answerItem[j_1].answered;
+                var from_1 = this._admin;
+                var amount_1 = Math.ceil(firstLevel/firstSize);
+                this.tokenTansfer(from_1,to_1,amount_1);
+            }
+
+            for (var j_2 = 0; j_2 < secondSize; j_2++){
+                var base_2 = j_2 + firstSize;
+                var to_2 = answerItem[base_2].answered;
+                var from_2 = this._admin;
+                var amount_2 = Math.ceil(secondLevel/secondSize);
+                this.tokenTansfer(from_2,to_2,amount_2);
+            }
+
+            for (var j_3 = 0; j_3<thirdSize; j_3++) {
+                var base_3 = j_3+firstSize+secondSize;
+                var to_3 = answerItem[base_3].answered;
+                var from_3 = this._admin;
+                var amount_3 = Math.ceil(thirdLevel/thirdSize);
+                this.tokenTansfer(from_3,to_3,amount_3);
+            }
+
+            var forthSize = new BigNumber(0);
+            for (var j_4 = 0;j<answerItem.length;j_4++){
+                forthSize = forthSize + answerItem[j].like.length + answerItem[j_4].dislike.length;
+            }
+            forthSize++;
+            var forthAmount = Math.ceil(forthLevel/forthSize);
+
+            for (var j_5 =0;j_5<answerItem.length;j_5++){
+
+                for (var i = 0; i<answerItem[j_5].like.length;i++){
+                    var like_to = answerItem[j_5].like[i];
+                    var like_from = this._admin;
+                    this.tokenTansfer(like_from,like_to,forthAmount);
+                }
+
+                for (var k =0; k<answerItem[j_5].dislike.length;k++) {
+                    var dislike_to = answerItem[j_5].dislike[i];
+                    var dislike_from = this._admin;
+                    this.tokenTansfer(dislike_from,dislike_to,forthAmount);
+                }
+
+            }
+
+            this.tokenTansfer(this._admin,challengeItem.author,forthAmount);
+
+        }
+        else {
+            throw new Error("Wait a minute.")
+        }
     }
+
+    // RewardAnswerFromPoster: function(challengeId) {
+    //
+    //     var answerItem = this.ChallengeValut.get(challengeId).answer;
+    //
+    //     var sortLike = function(a,b){
+    //
+    //         if (a.like.length < b.like.length) {
+    //             return 1;
+    //         }
+    //         else {
+    //             return -1;
+    //         }
+    //     };
+    //
+    //     answerItem.sort(sortLike);
+    //
+    //     for(var j = 0, length2 = 2; j < length2; j++){
+    //
+    //         if (answerItem[j].like > 2)
+    //          {
+    //             Event.Trigger("ChallengeValut", {
+    //                 Transfer: {
+    //                     from: Blockchain.transaction.from,
+    //                     to: answerItem[j].answered,
+    //                     value: Blockchain.transaction.value
+    //                 }
+    //             });
+    //          }
+    //     }
+    //
+    // },
+    //
+    // RewardChallenge: function(challengeId) {
+    //
+    //     var answerItem = this.ChallengeValut.get(challengeId).answer;
+    //     var author = this.ChallengeValut.get(challengeId).author;
+    //
+    //     if (answerItem.length > 2){
+    //
+    //         Event.Trigger("ChallengeValut", {
+    //             Transfer: {
+    //                 from: Blockchain.transaction.from,
+    //                 to: author,
+    //                 value: Blockchain.transaction.value
+    //             }
+    //         });
+    //     }
+    // }
 
 
 };
